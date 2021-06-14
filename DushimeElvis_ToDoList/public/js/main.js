62194d2d6529467061ee1604da43ab33ab7ee9a3
ElsongMaster <elsongmaster@gmail.com>
2021-06-14 11:24:38 +0200

    prototype première tâche créer

--- DushimeElvis_ToDoList/public/js/main.js
+++ DushimeElvis_ToDoList/public/js/main.js
@@ -27,4 +27,35 @@ container_inputAndButton.append(container_input, container_ButtonFilter);
 
 toDoListContent.append(title, container_inputAndButton);
 
+let container_CreatedTask = document.createElement("div");
+container_CreatedTask.className = "container_CreatedTask";
+let taskNode;
+let inputCheckBox;
+let labelInput;
+let div_InputTask = document.createElement("div");
+
+let div_Icon = document.createElement("div");
+div_Icon.innerHTML =
+  "<i class='fas fa-trash-alt'></i> <i class='fas fa-edit'></i>";
+div_Icon.className = "div_Icon";
+inputTaskName.addEventListener("keyup", (e) => {
+  if (e.key == "Enter") {
+    console.log(e.key);
+    inputCheckBox = document.createElement("input");
+    Object.assign(inputCheckBox, {
+      type: "checkbox",
+      id: `${inputTaskName.value}`,
+      name: `${inputTaskName.value}`,
+    });
+    labelInput = document.createElement("label");
+    labelInput.setAttribute("for", `${inputTaskName.value}`);
+    labelInput.textContent = `${inputTaskName.value}`;
+    taskNode = document.createElement("div");
+    taskNode.className = "taskNode";
+    div_InputTask.append(inputCheckBox, labelInput);
+    taskNode.append(div_InputTask, div_Icon);
+    container_CreatedTask.appendChild(taskNode);
+  }
+});
+toDoListContent.appendChild(container_CreatedTask);
 document.body.appendChild(toDoListContent);
